6f5eeb41673652f375e66af36b32dc84e9a19a4c
CTHartze <ismailmaho007@gmail.com>
2024-01-14 11:34:51 -0800

    updates route, new template, DOM javascript

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+/* Script listens for changes on each input checkbox tag and updates html */
+$(document).ready(function(e) {
+    let amenities = {}
+
+    $('.amenities input[type="checkbox"]').on('change', function(e) {
+        const id = $(this).attr('data-id');
+        const name = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            amenities[id] = name;
+        } else {
+            delete amenities[id];
+        }
+
+        $('.amenities h4').text(Object.values(amenities).join(', '))
+    })
+})
